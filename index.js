63465b1073c4f1c8e3231ebf3f4ddbd98fa172b5
MOHD AMIRUN AQMAR BIN ROSLAN <aqmar.roslan@compasia.com>
2023-06-21 21:09:32 +0800

    update deploymenet

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+require('dotenv').config();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/apikey", (req, res) => {
+
+  res.send(process.env.api_key);
 });
 
 app.listen(port, () => {
